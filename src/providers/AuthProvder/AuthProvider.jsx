d450ac718a9bdbc5d56f75412be72f3cc4dacc0d
Arifur Rahman <arifr956@gmail.com>
2023-11-08 20:17:01 +0600

    access token with jwt, axios, cors

--- src/providers/AuthProvder/AuthProvider.jsx
+++ src/providers/AuthProvder/AuthProvider.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../../firebase/firebase.config';
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
@@ -29,9 +30,26 @@ const AuthProvider = ({children}) => {
 
     useEffect(() =>{
         const unSubscribe = onAuthStateChanged(auth, currentUser =>{
-            console.log('user in current state',currentUser);
+            const userEmail = currentUser?.email || user?.email;
+            const loggedUser = { email: userEmail };
             setUser(currentUser);
+            console.log('current user', currentUser);
             setLoading(false);
+             // if user exists then issue a token
+             if (currentUser) {
+                axios.post('http://localhost:5000/jwt', loggedUser, { withCredentials: true })
+                    .then(res => {
+                        console.log('token response', res.data);
+                    })
+            }
+            else {
+                axios.post('http://localhost:5000/logout', loggedUser, {
+                    withCredentials: true
+                })
+                    .then(res => {
+                        console.log(res.data);
+                    })
+            }
         });
         return () =>{
             unSubscribe();
